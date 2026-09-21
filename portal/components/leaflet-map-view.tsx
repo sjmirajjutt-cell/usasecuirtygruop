7c2bf2b248e6630578b6c39734318c5b720ecb99
Ameer Hamza <digizaroclass@gmail.com>
2026-09-22 01:55:37 +0500

    Use Mapbox tiles for location map

--- portal/components/leaflet-map-view.tsx
+++ portal/components/leaflet-map-view.tsx
@@ -6,6 +6,14 @@ import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet'
 
 type Position = { lat: number; lng: number }
 
+const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN || ''
+const tileUrl = mapboxToken
+  ? `https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/512/{z}/{x}/{y}?access_token=${mapboxToken}`
+  : 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const attribution = mapboxToken
+  ? '&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  : '&copy; OpenStreetMap contributors'
+
 const markerIcon = new L.Icon({
   iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
   iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
@@ -26,7 +34,7 @@ export function LeafletMapView({ position, onMarkerMove }: { position: Position;
   return (
     <MapContainer center={[position.lat, position.lng]} zoom={13} style={{ height: '100%', width: '100%' }}>
       <MapUpdater position={position} />
-      <TileLayer attribution="&copy; OpenStreetMap contributors" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <TileLayer attribution={attribution} url={tileUrl} tileSize={mapboxToken ? 512 : 256} maxZoom={20} />
       <Marker position={[position.lat, position.lng]} icon={markerIcon} draggable eventHandlers={{ dragend: (event: L.DragEndEvent) => { const next = event.target.getLatLng(); onMarkerMove({ lat: next.lat, lng: next.lng }) } }} />
     </MapContainer>
   )
